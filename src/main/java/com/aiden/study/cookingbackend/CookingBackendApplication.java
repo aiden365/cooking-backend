f1883031d626edebffc2d5d09d3eefadf8d809b8
chenjiacheng1 <chenjiacheng365@qq.com>
2026-02-10 18:32:55 +0800

    初始提交

---
+++ src/main/java/com/aiden/study/cookingbackend/CookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aiden.study.cookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingBackendApplication.class, args);
+    }
+
+}
