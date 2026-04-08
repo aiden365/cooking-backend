3ee0369fd883ecac4a7b4edb4db363673e3e214c
aidenc <chenjiacheng365@qq.com>
2026-04-08 17:04:17 +0800

    这个人很懒，什么都没写。

---
+++ src/main/java/com/cooking/api/FileApi.java
@@ -0,0 +1,80 @@
+package com.cooking.api;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import com.cooking.base.BaseController;
+import com.cooking.base.BaseResponse;
+import com.cooking.enums.PathEnum;
+import com.cooking.exceptions.ApiException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 文件上传
+ */
+@RestController
+@RequestMapping("/file")
+public class FileApi extends BaseController {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Value("${upload.url}")
+    private String uploadUrl;
+
+    /**
+     * 文件上传
+     *
+     * @param file
+     * @param pathType
+     * @return
+     */
+    @PostMapping("/upload")
+    public BaseResponse upload(@RequestParam("file") MultipartFile file,@RequestParam("pathType") String pathType) {
+
+        if (StrUtil.isBlank(pathType)) {
+            return fail("pathType 不能为空");
+        }
+
+        PathEnum pathEnum = null;
+        for (PathEnum value : PathEnum.values()) {
+            if (value.toString().equals(pathType)) {
+                pathEnum = value;
+                break;
+            }
+        }
+
+        if (pathEnum == null) {
+            return fail("pathType 值不正确");
+        }
+
+        
+
+        // 获取文件的原始名称
+        String originalFilename = file.getOriginalFilename();
+        // 获取文件的扩展名
+        String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        // 生成新的文件名
+        String newFileName = IdUtil.simpleUUID() + fileExtension;
+        // 构建文件的存储路径
+        String filePath = pathEnum.getValue() + "/" + newFileName;
+        String destPath = uploadPath + filePath;
+
+        try {
+            // 保存文件
+            FileUtil.writeBytes(file.getBytes(), destPath);
+        } catch (IOException e) {
+            return fail("文件上传失败");
+        }
+
+        return ok(uploadUrl + filePath);
+    }
+}
--- src/main/java/com/cooking/config/WebMvcConfig.java
+++ src/main/java/com/cooking/config/WebMvcConfig.java
@@ -4,12 +4,15 @@ import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.support.config.FastJsonConfig;
 import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +22,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     private LoginInterceptor loginInterceptor;
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 配置静态资源映射
+        // 当请求 URL 以 /UploadFile/ 开头时，将其映射到 uploadPath 目录
+        registry.addResourceHandler("/UploadFile/**").addResourceLocations("file:" + uploadPath + "/UploadFile/");
+    }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
